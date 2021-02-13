0b9aab003b9dc99ffbcef66f313c886cc9c04c69
JaeHa <jaeha23@naver.com>
2021-02-14 07:35:36 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          바로 기릿 가나요가나요
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/바로 기릿 가나요가나요/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/바로 기릿 가나요가나요/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/바로 기릿 가나요가나요/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/바로 기릿 가나요가나요/i);
   expect(linkElement).toBeInTheDocument();
 });
